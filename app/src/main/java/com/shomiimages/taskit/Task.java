bb62262e361a9628a496152faa93eb01e1756b89
TheChientleGiant <thechientlegiant@gmail.com>
2016-03-12 13:37:44 -0800

    Initial commit

---
+++ app/src/main/java/com/shomiimages/taskit/Task.java
@@ -0,0 +1,36 @@
+package com.shomiimages.taskit;
+
+import java.util.Date;
+
+/**
+ * Created by Justin Dean Chien on 3/12/2016.
+ */
+public class Task {
+    private String mName;
+    private Date mDueDate;
+    private boolean mDone;
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void setDone(boolean done) {
+        mDone = done;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        mDueDate = dueDate;
+    }
+}
