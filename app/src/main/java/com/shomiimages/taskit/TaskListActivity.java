54a536b1ea91dc3871078000ed7d1a2702924605
TheChientleGiant <thechientlegiant@gmail.com>
2016-03-12 13:48:38 -0800

    Put in array to populate ListView

--- app/src/main/java/com/shomiimages/taskit/TaskListActivity.java
+++ app/src/main/java/com/shomiimages/taskit/TaskListActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TaskListActivity extends AppCompatActivity {
 
@@ -16,7 +18,10 @@ public class TaskListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
+        String[] items = {"1","2","3"};
 
+        ListView list = (ListView)findViewById(R.id.task_list);
+        list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
     }
 
     @Override
