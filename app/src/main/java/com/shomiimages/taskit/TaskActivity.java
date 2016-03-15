c0d93057e1c1a68e0dcf21df44bf168b35cc0809
TheChientleGiant <thechientlegiant@gmail.com>
2016-03-14 23:16:06 -0700

    Connecting task info elements to .java

--- app/src/main/java/com/shomiimages/taskit/TaskActivity.java
+++ app/src/main/java/com/shomiimages/taskit/TaskActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class TaskActivity extends AppCompatActivity {
 
@@ -19,7 +22,33 @@ public class TaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task);
 
         Task task = (Task) getIntent().getSerializableExtra(EXTRA);
-        Log.d(TAG, task.getName());
+
+        EditText taskNameInput = (EditText)findViewById(R.id.task_name);
+        Button dateButton = (Button)findViewById(R.id.task_date);
+        CheckBox doneBox = (CheckBox)findViewById(R.id.task_done);
+        Button saveButton = (Button)findViewById(R.id.save_button);
+
+        taskNameInput.setText(task.getName());
+        if (task.getDueDate()==null){
+            dateButton.setText(getResources().getString(R.string.no_date));
+        }else {
+            dateButton.setText(task.getDueDate().toString());
+        }
+        doneBox.setChecked(task.isDone());
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
